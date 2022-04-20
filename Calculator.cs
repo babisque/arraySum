ae098c50770889079c3b9dd0bdb15a16716c5a36
babisque <rodrigobabisque1@gmail.com>
2022-04-20 13:35:07 -0300

    Atividade pronta

---
+++ Calculator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula60Exe84
+{
+    internal class Calculator
+    {
+        public static int[] sumArrays(int[] arrayA, int[] arrayB)
+        {
+            return arrayA.Zip(arrayB, (x, y) => x + y).ToArray();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Aula60Exe84
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] odd = { 1, 3, 5, 7, 9 };
+            int[] pair = { 2, 4, 6, 8, 10 };
+
+            Calculator calcualtor = new Calculator();
+            int[] arraySum = Calculator.sumArrays(odd, pair);
+            
+            foreach (int result in arraySum)
+            {
+                Console.WriteLine(result);
+            }
+        }
+    }
+}
